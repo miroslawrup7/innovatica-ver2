6a429f610998bb2bf288511751af1f986f4d5d3a
Mirek Rup <miroslawrup7@gmail.com>
2022-07-20 06:06:33 +0200

    languages

--- src/js/main.js
+++ src/js/main.js
@@ -122,84 +122,244 @@ const next = document.querySelector(".nextPage");
 const galleryBox = document.querySelector(".gallery-box");
 const pagesLoc = document.querySelector(".pages");
 
-const pad = "000";
-let imgs = [];
+if (pagesLoc) {
+    const pad = "000";
+    let imgs = [];
 
-for (i = 0; i < 72; i++) {
-    let title =
-        pad.substring(0, pad.length - (i + 1).toString().length) +
-        (i + 1).toString();
-    imgs.push({
-        title: title,
-        source: `./img/gallery/${title}.jpg`,
-    });
-}
-
-const perPage = 12;
-let page = 1;
-let pagesNumber = Math.ceil(imgs.length / perPage);
-
-for (i = 0; i < pagesNumber; i++) {
-    const div = document.createElement("div");
-    const pageNumber = document.createTextNode(i + 1);
-    div.classList.add("page");
-    div.setAttribute("data-index", i);
-    div.appendChild(pageNumber);
-
-    div.addEventListener("click", function (e) {
-        goToPage(e.target.getAttribute("data-index"));
-    });
-
-    pagesLoc.appendChild(div);
-}
-
-previous.addEventListener("click", function () {
-    if (page === 1) {
-        page = 1;
-    } else {
-        page--;
-        showImages();
+    for (i = 0; i < 72; i++) {
+        let title =
+            pad.substring(0, pad.length - (i + 1).toString().length) +
+            (i + 1).toString();
+        imgs.push({
+            title: title,
+            source: `./img/gallery/${title}.jpg`,
+        });
     }
-});
 
-next.addEventListener("click", function () {
-    if (page < pagesNumber) {
-        page++;
-        showImages();
+    const perPage = 12;
+    let page = 1;
+    let pagesNumber = Math.ceil(imgs.length / perPage);
+
+    for (i = 0; i < pagesNumber; i++) {
+        const div = document.createElement("div");
+        const pageNumber = document.createTextNode(i + 1);
+        div.classList.add("page");
+        div.setAttribute("data-index", i);
+        div.appendChild(pageNumber);
+
+        div.addEventListener("click", function (e) {
+            goToPage(e.target.getAttribute("data-index"));
+        });
+
+        pagesLoc.appendChild(div);
     }
-});
 
-const goToPage = (index) => {
-    index = parseInt(index);
-    page = index + 1;
+    previous.addEventListener("click", function () {
+        if (page === 1) {
+            page = 1;
+        } else {
+            page--;
+            showImages();
+        }
+    });
+
+    next.addEventListener("click", function () {
+        if (page < pagesNumber) {
+            page++;
+            showImages();
+        }
+    });
+
+    const goToPage = (index) => {
+        index = parseInt(index);
+        page = index + 1;
+        showImages();
+    };
+
+    const showImages = () => {
+        while (galleryBox.firstChild) {
+            galleryBox.removeChild(galleryBox.firstChild);
+        }
+
+        let offset = (page - 1) * perPage;
+        const pages = document.querySelectorAll(".page");
+
+        for (i = 0; i < pages.length; i++) {
+            pages[i].classList.remove("active");
+        }
+
+        pages[page - 1].classList.add("active");
+
+        for (i = offset; i < offset + perPage; i++) {
+            if (imgs[i]) {
+                const img = document.createElement("img");
+
+                img.setAttribute("src", imgs[i].source);
+                img.setAttribute("alt", imgs[i].title);
+
+                gallery.appendChild(img);
+            }
+        }
+        lightboxSet();
+    };
+
     showImages();
-};
+}
 
-const showImages = () => {
-    while (galleryBox.firstChild) {
-        galleryBox.removeChild(galleryBox.firstChild);
+// LANGUAGES
+const language = document.querySelectorAll(".languages > li");
+
+language.forEach(function (el) {
+    el.addEventListener("click", (e) => {
+        let lang = e.target.dataset.lang;
+
+        saveLng(lang);
+
+        languageSet(lang);
+    });
+});
+
+const languageSet = (lang) => {
+    document.querySelectorAll(`[data-lang]`).forEach((el) => {
+        el.classList.remove("active");
+    });
+
+    if (lang) {
+        document.querySelector(`[data-lang=${lang}]`).classList.add("active");
+    } else {
+        document.querySelector('[data-lang="pl"]').classList.add("active");
     }
 
-    let offset = (page - 1) * perPage;
-    const pages = document.querySelectorAll(".page");
-
-    for (i = 0; i < pages.length; i++) {
-        pages[i].classList.remove("active");
+    for (let objKey in h_translate[lang]) {
+        document.getElementById(objKey).innerText = h_translate[lang][objKey];
     }
 
-    pages[page - 1].classList.add("active");
+    for (let objKey in q_translate[lang]) {
+        document.getElementById(objKey).innerText = q_translate[lang][objKey];
+    }
 
-    for (i = offset; i < offset + perPage; i++) {
-        if (imgs[i]) {
-            const img = document.createElement("img");
+    for (let objKey in f_translate[lang]) {
+        document.getElementById(objKey).innerText = f_translate[lang][objKey];
+    }
 
-            img.setAttribute("src", imgs[i].source);
-            img.setAttribute("alt", imgs[i].title);
+    if (document.body.dataset.page === "home") {
+        for (let objKey in ch_translate[lang]) {
+            document.getElementById(objKey).innerText =
+                ch_translate[lang][objKey];
+        }
 
-            gallery.appendChild(img);
+        for (let objKey in ch_translate_html[lang]) {
+            document.getElementById(objKey).innerHTML =
+                ch_translate_html[lang][objKey];
         }
     }
-    lightboxSet();
+
+    if (document.body.dataset.page === "about") {
+        for (let objKey in ca_translate[lang]) {
+            document.getElementById(objKey).innerText =
+                ca_translate[lang][objKey];
+        }
+
+        for (let objKey in ca_translate_html[lang]) {
+            document.getElementById(objKey).innerHTML =
+                ca_translate_html[lang][objKey];
+        }
+    }
+
+    if (document.body.dataset.page === "services") {
+        for (let objKey in cs_translate[lang]) {
+            document.getElementById(objKey).innerText =
+                cs_translate[lang][objKey];
+        }
+
+        for (let objKey in cs_translate_html[lang]) {
+            document.getElementById(objKey).innerHTML =
+                cs_translate_html[lang][objKey];
+        }
+        if (lang === "de") {
+            document
+                .querySelector(".services-2 .circle-imgs")
+                .classList.add("lng-de");
+            document
+                .querySelector(".services-2 .button")
+                .classList.add("lng-de");
+        } else {
+            document.querySelector(".circle-imgs").classList.remove("lng-de");
+            document
+                .querySelector(".services-2 .button")
+                .classList.remove("lng-de");
+        }
+    }
+
+    if (document.body.dataset.page === "portfolio") {
+        for (let objKey in cp_translate[lang]) {
+            document.getElementById(objKey).innerText =
+                cp_translate[lang][objKey];
+        }
+    }
+
+    if (document.body.dataset.page === "contact") {
+        for (let objKey in cc_translate[lang]) {
+            document.getElementById(objKey).innerText =
+                cc_translate[lang][objKey];
+        }
+    }
 };
 
-showImages();
+// IndexedDB
+
+const createDB = () => {
+    let openRequest = indexedDB.open("languageDB", 1);
+
+    openRequest.onupgradeneeded = function () {
+        let db = openRequest.result;
+        if (!db.objectStoreNames.contains("language")) {
+            db.createObjectStore("language", { autoIncrement: true });
+        }
+    };
+};
+
+createDB();
+
+const saveLng = (lng) => {
+    let openRequest = indexedDB.open("languageDB");
+
+    openRequest.onsuccess = function () {
+        let db = openRequest.result;
+        let transaction = db.transaction("language", "readwrite");
+        let languageTrans = transaction.objectStore("language");
+
+        let lang = lng;
+
+        let request = languageTrans.put(lang, "lang");
+
+        request.onsuccess = function () {};
+
+        request.onerror = function () {
+            console.log("Error", request.error);
+        };
+    };
+};
+
+const getLng = () => {
+    let openRequest = indexedDB.open("languageDB");
+
+    openRequest.onsuccess = function () {
+        let db = openRequest.result;
+        let transaction = db.transaction("language");
+        let languageTrans = transaction.objectStore("language");
+
+        let request = languageTrans.get("lang");
+
+        request.onsuccess = function () {
+            languageSet(request.result);
+        };
+
+        request.onerror = function () {
+            console.log("Error", request.error);
+        };
+    };
+};
+
+getLng();
+// languageSet();
